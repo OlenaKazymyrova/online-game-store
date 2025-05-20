a4f18c5db82f4af0c9341fea113f844498f875a0
Oleksii Stepanyk <o.stepanik06@gmail.com>
2025-05-20 21:24:09 +0300

    feature(db): migration with game entity created

---
+++ OnlineGameStore.DAL/Migrations/20250520182135_GameEntityAdded.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OnlineGameStore.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class GameEntityAdded : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "games",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    name = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: false),
+                    description = table.Column<string>(type: "nvarchar(max)", maxLength: 4096, nullable: false),
+                    publisher_id = table.Column<int>(type: "int", nullable: false),
+                    genre_id = table.Column<int>(type: "int", nullable: false),
+                    license_id = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_games", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "games");
+        }
+    }
+}
