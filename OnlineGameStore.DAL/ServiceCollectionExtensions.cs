ca2ca2fa3ef4447f74b69f0e81227dae7617b2ef
Oleksii Stepanyk <o.stepanik06@gmail.com>
2025-05-20 22:15:04 +0300

    feature(dal): added ServiceCollectionExtensions for DI

---
+++ OnlineGameStore.DAL/ServiceCollectionExtensions.cs
@@ -0,0 +1,20 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using OnlineGameStore.DAL.Interfaces;
+using OnlineGameStore.DAL.Repositories;
+
+namespace OnlineGameStore.DAL;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddDalServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<OnlineGameStoreDbContext>(options =>
+            options.UseSqlServer(configuration.GetConnectionString("ConnString"))); // Replace with the actual one
+
+        services.AddScoped<IGameRepository, GameRepository>();
+
+        return services;
+    }
+}
