9970f43e1135733a912ea006f2a54adf2fd42b41
Oleksii Stepanyk <o.stepanik06@gmail.com>
2025-05-20 17:28:01 +0300

    feature(dal): added generic repository

---
+++ OnlineGameStore.DAL/IRepository.cs
@@ -0,0 +1,10 @@
+namespace OnlineGameStore.DAL;
+
+public interface IRepository<T>
+{
+    Task<T?> GetByIdAsync(int id);
+    Task<IEnumerable<T>> GetAllAsync();
+    Task<T?> AddAsync(T entity);
+    Task<bool> UpdateAsync(T entity);
+    Task<bool> DeleteAsync(int id);
+}
