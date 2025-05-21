0449b6420879d2d863ab02a580859a52d363b605
Oleksii Stepanyk <o.stepanik06@gmail.com>
2025-05-21 20:23:09 +0300

    feature(bll): added IDataGenerator interface and GameDataGen class for tests

---
+++ OnlineGameStore.BLL.Tests/DataGenerators/GameDataGenerator.cs
@@ -0,0 +1,27 @@
+using OnlineGameStore.BLL.Tests.Interfaces;
+using OnlineGameStore.DAL.Entities;
+
+namespace OnlineGameStore.BLL.Tests.DataGenerators;
+
+public class GameDataGenerator : IDataGenerator<Game>
+{
+    public List<Game> Generate(int count)
+    {
+        var list = new List<Game>();
+
+        for (var i = 0; i < count; i++)
+        {
+            list.Add(new Game
+            {
+                Id = Guid.NewGuid(),
+                Name = $"Game {i}",
+                Description = $"Description {i}",
+                Publisher = Guid.NewGuid(),
+                Genre = Guid.NewGuid(),
+                License = Guid.NewGuid()
+            });
+        }
+
+        return list;
+    }
+}
