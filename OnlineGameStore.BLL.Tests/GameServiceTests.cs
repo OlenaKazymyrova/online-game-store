774d7581d23db04dbd6fdb5c0c98c74534b92945
Oleksii Stepanyk <o.stepanik06@gmail.com>
2025-05-21 20:24:18 +0300

    feature(bll-tests): added empty class for tests

---
+++ OnlineGameStore.BLL.Tests/GameServiceTests.cs
@@ -0,0 +1,23 @@
+﻿using OnlineGameStore.BLL.Services;
+using OnlineGameStore.BLL.Tests.DataGenerators;
+using OnlineGameStore.BLL.Tests.RepositoryMockCreator;
+
+namespace OnlineGameStore.BLL.Tests;
+
+public class GameServiceTests
+{
+    private const int EntityCount = 100;
+    private readonly GameService _gameService;
+
+    public GameServiceTests()
+    {
+        var gen = new GameDataGenerator();
+
+        var data = gen.Generate(EntityCount);
+        var repMock = new GameRepositoryMockCreator(data);
+
+        var mockRepository = repMock.Create();
+
+        _gameService = new GameService(mockRepository);
+    }
+}
