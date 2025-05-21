143ba48f9662569a84f83f3ffc448c1dbaaef2be
AnnN <anechytailenko@kse.org.ua>
2025-05-21 17:38:39 +0300

    add entity and configuration for License

---
+++ OnlineGameStore.DAL/Configurations/LicenseConfiguration.cs
@@ -0,0 +1,34 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using OnlineGameStore.DAL.Entities;
+
+namespace OnlineGameStore.DAL.Configurations;
+
+public class LicenseConfiguration : IEntityTypeConfiguration<License>
+{
+    public void Configure(EntityTypeBuilder<License> builder)
+    {
+        builder.ToTable("Licenses");
+        
+        builder.HasKey(l => l.Id);
+
+        builder.Property(l => l.GameId)
+            .IsRequired();
+
+        builder.Property(l => l.Description)
+            .HasMaxLength(500)
+            .IsUnicode(true);
+
+        builder.Property(l => l.Cost)
+            .HasColumnType("decimal(10, 2)")
+            .HasDefaultValue(0m);
+
+        builder.HasCheckConstraint("CK_License_Cost", "Cost>=0");
+        
+        builder.HasOne(l => l.Game).
+            WithMany(g => g.Licenses)
+            .HasForeignKey(l => l.GameId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+    }
+}
---
+++ OnlineGameStore.DAL/OnlineGameStoreDBContext.cs
@@ -0,0 +1,17 @@
+using System.Reflection;
+using Microsoft.EntityFrameworkCore;
+using OnlineGameStore.DAL.Entities;
+
+namespace OnlineGameStore.DAL;
+
+public class OnlineGameStoreDbContext(DbContextOptions<OnlineGameStoreDbContext> options) : DbContext(options)
+{
+    public DbSet<Game> Games { get; set; }
+    public DbSet<License> Licenses { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        base.OnModelCreating(modelBuilder);
+    }
+}
