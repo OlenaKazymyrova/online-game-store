204333bb1293baf668673a9f45804094c89af21c
Oleksii Stepanyk <o.stepanik06@gmail.com>
2025-05-23 01:37:04 +0300

    feature(bll): added mapper to ServiceCollection

--- OnlineGameStore.BLL/ServiceCollectionExtensions.cs
+++ OnlineGameStore.BLL/ServiceCollectionExtensions.cs
@@ -1,5 +1,6 @@
 using Microsoft.Extensions.DependencyInjection;
 using OnlineGameStore.BLL.Interfaces;
+using OnlineGameStore.BLL.Mapping;
 using OnlineGameStore.BLL.Services;
 
 namespace OnlineGameStore.BLL;
@@ -9,6 +10,7 @@ public static class ServiceCollectionExtensions
     public static IServiceCollection AddBllServices(this IServiceCollection services)
     {
         services.AddScoped<IGameService, GameService>();
+        services.AddAutoMapper(typeof(MappingProfile));
 
         return services;
     }
