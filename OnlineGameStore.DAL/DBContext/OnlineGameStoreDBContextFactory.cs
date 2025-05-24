4ca6af57f5ec98fdbeb76f4d7eaf62d90311bf40
Herman Havva <108483440+hermanhavva@users.noreply.github.com>
2025-05-24 16:15:36 +0300

    feature(DBContextFactory): add factory for easier migrations

---
+++ OnlineGameStore.DAL/DBContext/OnlineGameStoreDBContextFactory.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace OnlineGameStore.DAL.DBContext;
+
+public class OnlineGameStoreDbContextFactory : IDesignTimeDbContextFactory<OnlineGameStoreDbContext>
+{
+
+    public OnlineGameStoreDbContext CreateDbContext(string[] args)
+    {
+        var optionsBuilder = new DbContextOptionsBuilder<OnlineGameStoreDbContext>();
+
+        // use real connection string here when applying migrations
+        optionsBuilder.UseSqlServer("");
+
+        return new OnlineGameStoreDbContext(optionsBuilder.Options);
+    }
+}
