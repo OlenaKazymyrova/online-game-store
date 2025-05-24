1d6c918e8dc95f10f630dfadfe3e9efa021fc461
Oleksii Stepanyk <o.stepanik06@gmail.com>
2025-05-25 02:45:03 +0300

    feature(ui-tests): added tests for game controller

---
+++ OnlineGameStore.UI.Tests/GameControllerTests.cs
@@ -0,0 +1,42 @@
+﻿using System.Net;
+using System.Net.Http.Json;
+using OnlineGameStore.BLL.DTOs;
+
+namespace OnlineGameStore.UI.Tests;
+
+public class GameControllerTests(ControllerTestsHelper helper) : BaseControllerTests(helper)
+{
+    [Fact]
+    public async Task GetGameById_ShouldReturnGame_WhenExists()
+    {
+        var newGame = new GameDto
+        {
+            Name = "Game",
+            Description = "Game Description",
+            PublisherId = Guid.NewGuid(),
+            GenreId = Guid.NewGuid(),
+            LicenseId = Guid.NewGuid()
+        };
+        var game = await TestHelper.CreateRecordAsync<GameDto>(Client, "/games", newGame);
+        var gameId = game.Id;
+
+        var request = new HttpRequestMessage(HttpMethod.Get, $"/games/{gameId}");
+        var response = await Client.SendAsync(request);
+
+        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+
+        var fetchedDevice = await response.Content.ReadFromJsonAsync<GameDto>();
+        Assert.NotNull(fetchedDevice);
+        Assert.Equal("TP-Link", fetchedDevice.Name);
+    }
+
+    [Fact]
+    public async Task GetGameById_ShouldReturnNotFound_WhenGameDoesNotExist()
+    {
+        var newId = Guid.NewGuid().ToString();
+        var request = new HttpRequestMessage(HttpMethod.Get, "/games/" + newId);
+        var response = await Client.SendAsync(request);
+
+        Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
+    }
+}
