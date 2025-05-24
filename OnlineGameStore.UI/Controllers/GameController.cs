84487f816eeb6cdad1cb42affaff4e504deee984
Oleksii Stepanyk <o.stepanik06@gmail.com>
2025-05-25 02:49:04 +0300

    feature(ui): IGameService instead of IGameRepository

--- OnlineGameStore.UI/Controllers/GameController.cs
+++ OnlineGameStore.UI/Controllers/GameController.cs
@@ -1,22 +1,23 @@
 using Microsoft.AspNetCore.Mvc;
-using OnlineGameStore.DAL.Interfaces;
+using OnlineGameStore.BLL.DTOs;
+using OnlineGameStore.BLL.Interfaces;
 
 namespace OnlineGameStore.UI.Controllers;
 
 [Route("/games")]
 public class GameController : ControllerBase
 {
-    private readonly IGameRepository _repository;
+    private readonly IGameService _service;
 
-    public GameController(IGameRepository repository)
+    public GameController(IGameService service)
     {
-        _repository = repository;
+        _service = service;
     }
 
     [HttpGet("{id:guid}")]
     public async Task<IActionResult> GetByIdAsync(Guid id)
     {
-        var game = await _repository.GetByIdAsync(id);
+        var game = await _service.GetByIdAsync(id);
         if (game == null)
         {
             return NotFound();
