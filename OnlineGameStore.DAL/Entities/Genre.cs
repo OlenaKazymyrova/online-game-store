ae6c04eadea482ef6eb51831c3a789d82f3f8ac6
Herman Havva <108483440+hermanhavva@users.noreply.github.com>
2025-05-24 17:00:44 +0300

    refactor(Genre.cs)

--- OnlineGameStore.DAL/Entities/Genre.cs
+++ OnlineGameStore.DAL/Entities/Genre.cs
@@ -1,36 +1,35 @@
 ﻿using System;
 
-namespace OnlineGameStore.DAL.Entities
+namespace OnlineGameStore.DAL.Entities;
+
+public class Genre
 {
-    public class Genre
+    public Guid Id { get; set; } = Guid.NewGuid();
+    public required string Name { get; set; } = string.Empty;
+    public required string Description { get; set; } = string.Empty;
+    public Guid? ParentId { get; set; } = Guid.Empty;
+    public Genre? ParentGenre { get; set; } = default;
+
+    public override bool Equals(object? obj) =>
+        Equals(obj as Genre);
+
+    public override int GetHashCode() =>
+        HashCode.Combine(Id, Name, Description, ParentId);
+
+    public static bool operator ==(Genre? left, Genre? right) =>
+        Equals(left, right);
+
+    public static bool operator !=(Genre? left, Genre? right) =>
+        !Equals(left, right);
+
+    private bool Equals(Genre? other)
     {
-        public Guid Id { get; set; } = Guid.NewGuid();
-        public required string Name { get; set; } = string.Empty;
-        public required string Description { get; set; } = string.Empty;
-        public Guid? ParentId { get; set; } = Guid.Empty;
-        public Genre? ParentGenre { get; set; } = default;
+        if (other is null) return false;
+        if (ReferenceEquals(this, other)) return true;
 
-        public override bool Equals(object? obj) =>
-            Equals(obj as Genre);
-
-        public override int GetHashCode() =>
-            HashCode.Combine(Id, Name, Description, ParentId);
-
-        public static bool operator ==(Genre? left, Genre? right) =>
-            Equals(left, right);
-
-        public static bool operator !=(Genre? left, Genre? right) =>
-            !Equals(left, right);
-
-        private bool Equals(Genre? other)
-        {
-            if (other is null) return false;
-            if (ReferenceEquals(this, other)) return true;
-
-            return Id == other.Id
-                && Name == other.Name
-                && Description == other.Description
-                && ParentId == other.ParentId;
-        }
+        return Id == other.Id
+            && Name == other.Name
+            && Description == other.Description
+            && ParentId == other.ParentId;
     }
 }
