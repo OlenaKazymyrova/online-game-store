5c21693e5ff5b9520974f173d085b1a63bd527d3
Herman Havva <108483440+hermanhavva@users.noreply.github.com>
2025-05-26 19:32:51 +0300

    feature(GenreDTO): add `GenreDto.cs`
    Following Fields marked required as for now:
    - Id
    - Name
    -Description

---
+++ OnlineGameStore.BLL/DTOs/GenreDto.cs
@@ -0,0 +1,9 @@
+﻿namespace OnlineGameStore.BLL.DTOs;
+
+public class GenreDto
+{
+    public required Guid Id { get; set; }
+    public required string Name { get; set; }
+    public required string Description {  get; set; }
+    public Guid? ParentId { get; set; }
+}
