2e11ce4c7560ca41a104989d1528973b26617ee5
AnnN <anechytailenko@kse.org.ua>
2025-05-26 12:36:18 +0300

    resolve conflict

--- OnlineGameStore.DAL/Repositories/LicenseRepository.cs
+++ OnlineGameStore.DAL/Repositories/LicenseRepository.cs
@@ -6,11 +6,9 @@ namespace OnlineGameStore.DAL.Repositories;
 
 public class LicenseRepository : GenericRepository<License>, ILicenseRepository
 {
-<<<<<<< HEAD
-    public LicenseRepository(OnlineGameStoreDbContextFactory dbContextFactory) : base(dbContextFactory)
-=======
+
     public LicenseRepository(IOnlineGameStoreDbContextFactory dbContextFactory) : base(dbContextFactory)
->>>>>>> feature/license
+
     {
 
     }
