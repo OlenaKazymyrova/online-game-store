7f1394d1b9106b17f55b59f9083b518e841a8cfb
AnnN <anechytailenko@kse.org.ua>
2025-05-26 15:56:59 +0300

    format code

--- OnlineGameStore.BLL/DTOs/License/LicenseDto.cs
+++ OnlineGameStore.BLL/DTOs/License/LicenseDto.cs
@@ -5,8 +5,8 @@ namespace OnlineGameStore.BLL.DTOs;
 public class LicenseDto
 {
     public string Description { get; set; }
-    
+
     public decimal Cost { get; set; }
-    
+
     public Guid GameId { get; set; }
 }
