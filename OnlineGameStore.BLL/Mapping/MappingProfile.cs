738eb80c9094618bbc5114f2737e99d453f7a5a5
Herman Havva <108483440+hermanhavva@users.noreply.github.com>
2025-05-26 19:34:10 +0300

    feature(MappingProfile): update mapping profile for Genre

--- OnlineGameStore.BLL/Mapping/MappingProfile.cs
+++ OnlineGameStore.BLL/Mapping/MappingProfile.cs
@@ -15,5 +15,12 @@ public class MappingProfile : Profile
             .ForMember(dest => dest.PublisherId, opt => opt.MapFrom(src => src.Publisher))
             .ForMember(dest => dest.GenreId, opt => opt.MapFrom(src => src.Genre))
             .ForMember(dest => dest.LicenseId, opt => opt.MapFrom(src => src.License));
+
+
+        CreateMap<Genre, GenreDto>().ReverseMap()  
+            .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id))
+            .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Name))
+            .ForMember(dest => dest.Description, opt => opt.MapFrom(src => src.Description))
+            .ForMember(dest => dest.ParentId, opt => opt.MapFrom(src => src.ParentId));
     }
 }
