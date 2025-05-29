4493c227709407849112bdd2804f2cb76716bcba
Herman Havva <108483440+hermanhavva@users.noreply.github.com>
2025-05-29 22:10:02 +0300

    feature(GenreDto): add equality operator

--- OnlineGameStore.BLL/DTOs/GenreDto.cs
+++ OnlineGameStore.BLL/DTOs/GenreDto.cs
@@ -1,4 +1,6 @@
-﻿namespace OnlineGameStore.BLL.DTOs;
+﻿using OnlineGameStore.DAL.Entities;
+
+namespace OnlineGameStore.BLL.DTOs;
 
 public class GenreDto
 {
@@ -6,4 +8,28 @@ public class GenreDto
     public required string Name { get; set; }
     public required string Description {  get; set; }
     public Guid? ParentId { get; set; }
+
+
+    public override bool Equals(object? obj) =>
+        Equals(obj as GenreDto);
+
+    public override int GetHashCode() =>
+        HashCode.Combine(Id, Name, Description, ParentId);
+
+    public static bool operator ==(GenreDto? left, GenreDto? right) =>
+        Equals(left, right);
+
+    public static bool operator !=(GenreDto? left, GenreDto? right) =>
+        !Equals(left, right);
+
+    private bool Equals(GenreDto? other)
+    {
+        if (other is null) return false;
+        if (ReferenceEquals(this, other)) return true;
+
+        return Id == other.Id &&
+            Name == other.Name &&
+            Description == other.Description &&
+            ParentId == other.ParentId;
+    }
 }
