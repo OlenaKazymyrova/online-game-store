1faba5dcc40ca4a14f9a9cc2faaac685bd66c101
Oleksii Stepanyk <o.stepanik06@gmail.com>
2025-06-01 20:00:55 +0300

    chore(ui): added XML comments for GET methods

--- OnlineGameStore.UI/Controllers/GamesController.cs
+++ OnlineGameStore.UI/Controllers/GamesController.cs
@@ -15,7 +15,35 @@ public class GamesController : ControllerBase
         _service = service ?? throw new ArgumentNullException(nameof(service));
     }
 
+    /// <summary>
+    /// Returns a Game entity by its ID.
+    /// </summary>
+    /// <remarks>
+    /// Sample request:
+    ///
+    ///     GET api/Games/fd5f3b67-0187-48ee-a50e-afdfd1ce5830
+    ///
+    /// Returns:
+    /// 
+    ///     {
+    ///         "id": fd5f3b67-0187-48ee-a50e-afdfd1ce5830,
+    ///         "name": "Sample Game",
+    ///         "description": "This is a sample game description.",
+    ///         "publisherId": "05de777a-05fb-4792-bab5-0b171b6e5be4",
+    ///         "genreId": "7d606f4b-e3c8-4d89-8b24-8dc121945bf6",
+    ///         "licenseId": "259fe8d4-840e-4680-b969-aefc7f527118",
+    ///         "price": 29.99,
+    ///         "releaseDate": "2023-10-01T00:00:00"
+    ///     }
+    /// </remarks>
+    /// <param name="id">ID of a Game entity to get</param>
+    /// <returns>Game with specified ID if exists</returns>
+    /// <response code="200">Returns the Game entity</response>
+    /// <response code="404">If the Game with specified ID does not exist</response>
     [HttpGet("{id:guid}")]
+    [ProducesResponseType(typeof(GameDto), 200)]
+    [Produces("application/json")]
+    [ProducesResponseType(typeof(void), 404)]
     public async Task<IActionResult> GetByIdAsync(Guid id)
     {
         var game = await _service.GetByIdAsync(id);
@@ -27,7 +55,43 @@ public class GamesController : ControllerBase
         return Ok(game);
     }
 
+    /// <summary>
+    /// Returns list of all Game entities.
+    /// </summary>
+    /// <remarks>
+    /// Sample request:
+    ///
+    ///     GET api/Games
+    ///
+    /// Returns:
+    /// 
+    ///     [
+    ///         {
+    ///             "id": "fd5f3b67-0187-48ee-a50e-afdfd1ce5830",
+    ///             "name": "Sample Game 1",
+    ///             "description": "This is a sample game description.",
+    ///             "publisherId": "05de777a-05fb-4792-bab5-0b171b6e5be4",
+    ///             "genreId": "7d606f4b-e3c8-4d89-8b24-8dc121945bf6",
+    ///             "licenseId": "259fe8d4-840e-4680-b969-aefc7f527118",
+    ///             "price": 29.99,
+    ///             "releaseDate": "2023-10-01T00:00:00"
+    ///         },
+    ///         {
+    ///             "id": "a7e4d5cc-3c7d-4cf4-b23a-eaf91c03b192",
+    ///             "name": "Sample Game 2",
+    ///             "description": "Another sample game description.",
+    ///             "publisherId": "1c2d3e4f-5678-4f9b-9abc-def123456789",
+    ///             "genreId": "abcde123-4567-8901-2345-abcdef678901",
+    ///             "licenseId": "76543210-fedc-ba98-7654-3210fedcba98",
+    ///             "price": 49.99,
+    ///             "releaseDate": "2024-06-15T00:00:00"
+    ///         }
+    ///     ]
+    /// </remarks>
+    /// <returns>List of all Game entities</returns>
     [HttpGet]
+    [ProducesResponseType(typeof(IEnumerable<GameDto>), 200)]
+    [Produces("application/json")]
     public async Task<IActionResult> GetAllAsync()
     {
         var games = await _service.GetAllAsync();
