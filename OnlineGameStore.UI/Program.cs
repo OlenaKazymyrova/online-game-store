a22f215d9aa0c8f24e05e727eb3eead088ada407
Oleksii Stepanyk <o.stepanik06@gmail.com>
2025-06-01 18:45:28 +0300

    chore(ui): removed openapi from Program.cs

--- OnlineGameStore.UI/Program.cs
+++ OnlineGameStore.UI/Program.cs
@@ -3,9 +3,6 @@ using OnlineGameStore.DAL;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
 builder.Services.AddControllers();
 builder.Services.AddDalServices(builder.Configuration);
 builder.Services.AddBllServices();
@@ -15,7 +12,6 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
 }
 
 app.UseHttpsRedirection();
