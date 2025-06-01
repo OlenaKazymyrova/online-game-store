6cd452d57719e5ad2825a55a8057b7d476ec46c2
Herman Havva <108483440+hermanhavva@users.noreply.github.com>
2025-06-01 13:09:30 +0300

    feature(`GenreController`): add GET and POST
    Note: POST generates a Guid if it was not specified in the request body

--- OnlineGameStore.UI/Controllers/GenresController.cs
+++ OnlineGameStore.UI/Controllers/GenresController.cs
@@ -1,6 +1,64 @@
-﻿namespace OnlineGameStore.UI.Controllers
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using OnlineGameStore.BLL.DTOs;
+using OnlineGameStore.BLL.Interfaces;
+
+namespace OnlineGameStore.UI.Controllers;
+
+
+[Route("[Controller]")]
+[ApiController]
+public class GenresController : ControllerBase
 {
-    public class GenresController
+    private readonly IGenreService _service;
+
+    public GenresController(IGenreService service)
     {
+        _service = service;
+    }
+
+    [HttpGet("{id:guid}")]
+    public async Task<IActionResult> GetById(Guid id)
+    {
+        var dto = await _service.GetByIdAsync(id);
+
+        return (dto is null) ? NotFound() : Ok(dto);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create([FromBody] GenreDto dto)
+    {
+        if (dto is null)
+        {
+            return BadRequest("Genre data is invalid or not present");
+        }
+
+        if (dto.Id is null
+            || dto.Id == Guid.Empty)
+        {
+            dto.Id = Guid.NewGuid();
+        }
+
+        // ###############
+        // this is to be handled with filters in the future
+        var genres = await _service.GetAllAsync();
+        if (genres.Any(existing => existing.Equals(dto)))
+        {
+            return Conflict("The genre already exists");
+        }
+        // ###############
+
+        var createdGenre = await _service.AddAsync(dto);
+
+        if (createdGenre is null)
+        {
+            return StatusCode(500);  // or should I throw a BadRequest() ?
+        }
+
+        return CreatedAtAction(
+            nameof(GetById),
+            new { id = createdGenre.Id },
+            createdGenre
+        );
     }
 }
