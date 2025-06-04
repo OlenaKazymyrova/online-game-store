a1db21dfa7efd323f47289f6ed6123bcfd80a719
Oleksii Stepanyk <o.stepanik06@gmail.com>
2025-06-04 19:45:04 +0300

    feature(dal-tests): override DeleteAsync method in GenreServiceMockCreator

--- OnlineGameStore.UI.Tests/ServiceMockCreators/GenreServiceMockCreator.cs
+++ OnlineGameStore.UI.Tests/ServiceMockCreators/GenreServiceMockCreator.cs
@@ -9,4 +9,28 @@ namespace OnlineGameStore.UI.Tests.ServiceMockCreators;
 public class GenreServiceMockCreator : ServiceMockCreator<Genre, GenreDto, IGenreService>
 {
     public GenreServiceMockCreator(List<GenreDto> data) : base(data) { }
+
+    protected override void SetupDelete(Mock<IGenreService> mock)
+    {
+        mock.Setup(x => x.DeleteAsync(It.IsAny<Guid>()))
+            .ReturnsAsync((Guid id) =>
+            {
+                var index = Data.FindIndex(x => x.Id == id);
+                if (index == -1)
+                {
+                    return false;
+                }
+
+                Data.RemoveAt(index);
+
+                var parentRefToRemove = id;
+
+                foreach (var item in Data.Where(g => g.ParentId == parentRefToRemove))
+                {
+                    item.ParentId = null;
+                }
+
+                return true;
+            });
+    }
 }
