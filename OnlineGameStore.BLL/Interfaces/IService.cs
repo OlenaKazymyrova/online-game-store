b9f532c1f76aa9e7443bcaa8f55bb9021691f88d
Herman Havva <108483440+hermanhavva@users.noreply.github.com>
2025-06-07 19:40:15 +0300

    feature: update interfaces to handle pagination

--- OnlineGameStore.BLL/Interfaces/IService.cs
+++ OnlineGameStore.BLL/Interfaces/IService.cs
@@ -1,5 +1,6 @@
 ﻿using System.Linq.Expressions;
 using Microsoft.EntityFrameworkCore.Query;
+using OnlineGameStore.SharedLogic.Pagination;
 
 namespace OnlineGameStore.BLL.Interfaces;
 
@@ -10,10 +11,11 @@ public interface IService<TEntity, TCreateDto, TReadDto, TUpdateDto>
     where TUpdateDto : class
 {
     Task<TReadDto?> GetByIdAsync(Guid id);
-    Task<IEnumerable<TReadDto>> GetAsync(
+    Task<PaginatedResponse<TReadDto>> GetAsync(
         Expression<Func<TEntity, bool>>? filter = null,
         Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>>? orderBy = null,
-        Func<IQueryable<TEntity>, IIncludableQueryable<TEntity, object>>? include = null);
+        Func<IQueryable<TEntity>, IIncludableQueryable<TEntity, object>>? include = null,
+        PagingParams? pagingParams = null);
     Task<TReadDto?> AddAsync(TCreateDto dto);
     Task<bool> UpdateAsync(TUpdateDto dto);
     Task<bool> DeleteAsync(Guid id);
