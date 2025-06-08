8c409c371101c65fcab076f0d5efafeda78d813d
Oleksii Stepanyk <o.stepanik06@gmail.com>
2025-06-08 17:25:23 +0300

    feature(bll): removed unnecessary explicit mapping

--- OnlineGameStore.BLL/Mapping/MappingProfile.cs
+++ OnlineGameStore.BLL/Mapping/MappingProfile.cs
@@ -8,16 +8,7 @@ public class BllMappingProfile : Profile
 {
     public BllMappingProfile()
     {
-        CreateMap<Game, GameDto>()
-            .ForMember(dest => dest.PublisherId, opt => opt.MapFrom(src => src.PublisherId))
-            .ForMember(dest => dest.GenreId, opt => opt.MapFrom(src => src.GenreId))
-            .ForMember(dest => dest.LicenseId, opt => opt.MapFrom(src => src.LicenseId))
-            .ReverseMap()
-            .ForMember(dest => dest.PublisherId, opt => opt.MapFrom(src => src.PublisherId))
-            .ForMember(dest => dest.GenreId, opt => opt.MapFrom(src => src.GenreId))
-            .ForMember(dest => dest.LicenseId, opt => opt.MapFrom(src => src.LicenseId))
-            .ForMember(dest => dest.Price, opt => opt.MapFrom(src => src.Price))
-            .ForMember(dest => dest.ReleaseDate, opt => opt.MapFrom(src => src.ReleaseDate));
+        CreateMap<Game, GameDto>().ReverseMap();
 
         CreateMap<Genre, GenreDto>().ReverseMap();
 
