8a3fc35f8594a71867d56fe4b21f74bf986d1f25
AnnN <anechytailenko@kse.org.ua>
2025-06-11 10:45:16 +0300

    fix TEntity inheritance in Platform_DAL

--- OnlineGameStore.DAL/Entities/Platform.cs
+++ OnlineGameStore.DAL/Entities/Platform.cs
@@ -1,8 +1,8 @@
 using OnlineGameStore.DAL.Entities;
 
-public class Platform
+public class Platform : TEntity
 {
-    public Guid Id { get; set; } = Guid.NewGuid();
+    public override Guid Id { get; set; } = Guid.NewGuid();
     public string Name { get; set; } = string.Empty;
     public ICollection<Game> Games { get; set; } = new List<Game>();
 }
