3605e5c48a2c710d6a859cb1eac49eee2f2a15c3
Herman Havva <108483440+hermanhavva@users.noreply.github.com>
2025-06-11 14:41:43 +0300

    refactor mapping profile

--- OnlineGameStore.BLL/Mapping/MappingProfile.cs
+++ OnlineGameStore.BLL/Mapping/MappingProfile.cs
@@ -8,26 +8,34 @@ public class BllMappingProfile : Profile
 {
     public BllMappingProfile()
     {
+        // #####################################
+        // # Game
+        // #####################################
         CreateMap<Game, GameDto>().ReverseMap();
 
         CreateMap<GameCreateDto, Game>()
             .ForMember(dest => dest.Id, opt => opt.MapFrom(_ => Guid.NewGuid()))
             .ForMember(dest => dest.Platforms, opt => opt.Ignore());
 
+        // #####################################
+        // # Genre
+        // #####################################
         CreateMap<Genre, GenreDto>().ReverseMap();
 
         CreateMap<Genre, GenreReadDto>().ReverseMap();
 
+        CreateMap<GenreCreateDto, Genre>()
+            .ForMember(dest => dest.Id, opt => opt.MapFrom(_ => Guid.NewGuid()))
+            .ForMember(dest => dest.ParentGenre, opt => opt.Ignore());
+
+        // #####################################
+        // # Platform
+        // #####################################
         CreateMap<Platform, PlatformDto>();
 
         CreateMap<PlatformCreateDto, Platform>()
             .ForMember(dest => dest.Id, opt => opt.MapFrom(_ => Guid.NewGuid()))
             .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Name.Trim()))
             .ForMember(dest => dest.Games, opt => opt.Ignore());
-
-        CreateMap<GenreCreateDto, Genre>()
-            .ForMember(dest => dest.Id, opt => opt.MapFrom(_ => Guid.NewGuid()))
-            .ForMember(dest => dest.ParentGenre, opt => opt.Ignore());
-
     }
 }
