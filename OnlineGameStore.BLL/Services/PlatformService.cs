6e0aefcbe70241dac3db8d79bac87fca88fe5922
AnnN <anechytailenko@kse.org.ua>
2025-06-11 11:10:55 +0300

    override add and update method in platform service

--- OnlineGameStore.BLL/Services/PlatformService.cs
+++ OnlineGameStore.BLL/Services/PlatformService.cs
@@ -8,7 +8,37 @@ namespace OnlineGameStore.BLL.Services;
 
 public class PlatformService : Service<Platform, PlatformCreateDto, PlatformDto, PlatformDto>, IPlatformService
 {
-    public PlatformService(IPlatformRepository repository, IMapper mapper)
-        : base(repository, mapper) { }
+    public PlatformService(IRepository<Platform> repository, IMapper mapper)
+        : base(repository, mapper)
+    {
+    }
 
+    public override async Task<PlatformDto?> AddAsync(PlatformCreateDto dto)
+    {
+        var entity = _mapper.Map<Platform>(dto);
+
+        var existing = await _repository.GetAsync(
+            filter: p => p.Name.ToLower() == entity.Name.ToLower()
+        );
+
+        if (existing.Items.Any()) return null;
+
+        var addedEntity = await _repository.AddAsync(entity);
+
+        return _mapper.Map<PlatformDto>(addedEntity);
+    }
+
+    public override async Task<bool> UpdateAsync(Guid id, PlatformCreateDto dto)
+    {
+        var entity = _mapper.Map<Platform>(dto);
+        entity.Id = id;
+
+        var existing = await _repository.GetAsync(
+            filter: p => p.Name.ToLower() == entity.Name.ToLower()
+        );
+
+        if (existing.Items.Any()) return false;
+
+        return await _repository.UpdateAsync(entity);
+    }
 }
