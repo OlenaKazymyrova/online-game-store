ac1d61b8202d861394f7aec0068b8df4854567f8
Oleksii Stepanyk <o.stepanik06@gmail.com>
2025-06-13 17:42:45 +0300

    feature(ui): added seeder service for seeding data

---
+++ OnlineGameStore.UI/Services/RoleSeederService.cs
@@ -0,0 +1,9 @@
+namespace OnlineGameStore.UI.Services;
+
+public class RoleSeederService : BackgroundService
+{
+    protected override Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        throw new NotImplementedException();
+    }
+}
