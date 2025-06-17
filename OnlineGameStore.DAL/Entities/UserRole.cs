b14efb8508814303501ea48eb9404768f53ae86f
Oleksii Stepanyk <o.stepanik06@gmail.com>
2025-06-17 13:09:04 +0300

    feature(dal): added Role and User entity in UserRole for connectivity and queries

--- OnlineGameStore.DAL/Entities/UserRole.cs
+++ OnlineGameStore.DAL/Entities/UserRole.cs
@@ -3,5 +3,7 @@ namespace OnlineGameStore.DAL.Entities;
 public class UserRole
 {
     public Guid UserId { get; set; }
+    public User User { get; set; }
     public Guid RoleId { get; set; }
+    public Role Role { get; set; }
 }
