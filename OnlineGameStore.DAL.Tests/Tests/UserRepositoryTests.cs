c7fba568abf40062a7b5074c3055cf89d6ed442c
Oleksii Stepanyk <o.stepanik06@gmail.com>
2025-06-18 15:16:51 +0300

    feature(dal): renamed role to user in UserRepositoryTests

--- OnlineGameStore.DAL.Tests/Tests/UserRepositoryTests.cs
+++ OnlineGameStore.DAL.Tests/Tests/UserRepositoryTests.cs
@@ -11,31 +11,31 @@ public class UserRepositoryTests
     public async Task GetAsync_ReturnsAllRoles()
     {
         var repository = _creator.Create();
-        var role1 = GetUser("User 1", "cool@example.com");
-        var role2 = GetUser("User 2", "john@example.com");
+        var user1 = GetUser("User 1", "cool@example.com");
+        var user2 = GetUser("User 2", "john@example.com");
 
-        await repository.AddAsync(role1);
-        await repository.AddAsync(role2);
+        await repository.AddAsync(user1);
+        await repository.AddAsync(user2);
 
         var result = await repository.GetAsync();
 
         Assert.NotNull(result);
         Assert.Equal(2, result.Items.Count());
-        Assert.Contains(result.Items, r => r.UserName == role1.UserName);
-        Assert.Contains(result.Items, r => r.UserName == role2.UserName);
+        Assert.Contains(result.Items, u => u.UserName == user1.UserName);
+        Assert.Contains(result.Items, u => u.UserName == user2.UserName);
     }
 
     [Fact]
     public async Task ExistsAsync_RoleExists_ReturnsTrue()
     {
         var repository = _creator.Create();
-        var role = GetUser();
+        var user = GetUser();
 
-        var addedRole = await repository.AddAsync(role);
+        var addedUser = await repository.AddAsync(user);
 
-        Assert.NotNull(addedRole);
+        Assert.NotNull(addedUser);
 
-        var result = await repository.ExistsAsync(addedRole!.Id);
+        var result = await repository.ExistsAsync(addedUser!.Id);
 
         Assert.True(result);
     }
@@ -54,25 +54,25 @@ public class UserRepositoryTests
     public async Task GetByIdAsync_RoleExists_ReturnsRole()
     {
         var repository = _creator.Create();
-        var role = GetUser();
+        var user = GetUser();
 
-        var addedRole = await repository.AddAsync(role);
+        var addedUser = await repository.AddAsync(user);
 
-        Assert.NotNull(addedRole);
+        Assert.NotNull(addedUser);
 
-        var result = await repository.GetByIdAsync(addedRole!.Id);
+        var result = await repository.GetByIdAsync(addedUser!.Id);
 
         Assert.NotNull(result);
-        Assert.Equal(addedRole, result);
+        Assert.Equal(addedUser, result);
     }
 
     [Fact]
     public async Task GetByIdAsync_RoleDoesNotExits_ReturnsNull()
     {
         var repository = _creator.Create();
-        var role = GetUser();
+        var user = GetUser();
 
-        var result = await repository.GetByIdAsync(role.Id);
+        var result = await repository.GetByIdAsync(user.Id);
 
         Assert.Null(result);
     }
@@ -81,60 +81,60 @@ public class UserRepositoryTests
     public async Task AddAsync_AddsRole()
     {
         var repository = _creator.Create();
-        var role = GetUser();
-        var result = await repository.AddAsync(role);
+        var user = GetUser();
+        var result = await repository.AddAsync(user);
 
         Assert.NotNull(result);
         Assert.True(result.Id != Guid.Empty);
-        Assert.Equal(role, result);
+        Assert.Equal(user, result);
     }
 
     [Fact]
     public async Task UpdateAsync_RoleExists_UpdatesRole()
     {
         var repository = _creator.Create();
-        var role = GetUser();
+        var user = GetUser();
         const string newUsername = "UpdatedName";
         const string newEmail = "new@example.com";
 
-        var addedRole = await repository.AddAsync(role);
+        var addedUser = await repository.AddAsync(user);
 
-        Assert.NotNull(addedRole);
+        Assert.NotNull(addedUser);
 
-        addedRole.UserName = newUsername;
-        addedRole.Email = newEmail;
+        addedUser.UserName = newUsername;
+        addedUser.Email = newEmail;
 
-        var result = await repository.UpdateAsync(addedRole);
+        var result = await repository.UpdateAsync(addedUser);
 
         Assert.True(result);
 
-        var updatedRole = await repository.GetByIdAsync(addedRole.Id);
+        var updatedUser = await repository.GetByIdAsync(addedUser.Id);
 
-        Assert.NotNull(updatedRole);
-        Assert.Equal(newUsername, updatedRole!.UserName);
-        Assert.Equal(newEmail, updatedRole.Email);
+        Assert.NotNull(updatedUser);
+        Assert.Equal(newUsername, updatedUser!.UserName);
+        Assert.Equal(newEmail, updatedUser.Email);
     }
 
     [Fact]
     public async Task UpdateAsync_RoleDoesNotExist_ReturnsException()
     {
         var repository = _creator.Create();
-        var role = GetUser();
+        var user = GetUser();
 
-        await Assert.ThrowsAnyAsync<Exception>(async () => await repository.UpdateAsync(role));
+        await Assert.ThrowsAnyAsync<Exception>(async () => await repository.UpdateAsync(user));
     }
 
     [Fact]
     public async Task DeleteAsync_RoleExists_DeletesRole()
     {
         var repository = _creator.Create();
-        var role = GetUser();
+        var user = GetUser();
 
-        var addedRole = await repository.AddAsync(role);
+        var addedUser = await repository.AddAsync(user);
 
-        Assert.NotNull(addedRole);
+        Assert.NotNull(addedUser);
 
-        var result = await repository.DeleteAsync(addedRole.Id);
+        var result = await repository.DeleteAsync(addedUser.Id);
 
         Assert.True(result);
     }
