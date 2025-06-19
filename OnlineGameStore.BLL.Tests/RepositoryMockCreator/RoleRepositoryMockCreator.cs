ebd5ab7c3ac8f10e3e11850f399accb2c2fb99a7
Oleksii Stepanyk <o.stepanik06@gmail.com>
2025-06-19 18:52:54 +0300

    feature(bll-tests): setup relation between user/role and userrole entities in mock

--- OnlineGameStore.BLL.Tests/RepositoryMockCreator/RoleRepositoryMockCreator.cs
+++ OnlineGameStore.BLL.Tests/RepositoryMockCreator/RoleRepositoryMockCreator.cs
@@ -1,3 +1,4 @@
+using Moq;
 using OnlineGameStore.DAL.Entities;
 using OnlineGameStore.DAL.Interfaces;
 
@@ -5,8 +6,31 @@ namespace OnlineGameStore.BLL.Tests.RepositoryMockCreator;
 
 public class RoleRepositoryMockCreator : RepositoryMockCreator<Role, IRoleRepository>
 {
-    public RoleRepositoryMockCreator(List<Role> roles)
+    private readonly IUserRoleRepository _userRoleRepository;
+
+    public RoleRepositoryMockCreator(List<Role> roles, IUserRoleRepository userRoleRepository)
         : base(roles)
     {
+        _userRoleRepository = userRoleRepository ?? throw new ArgumentNullException(nameof(userRoleRepository));
+    }
+
+    protected override void SetupDelete(Mock<IRoleRepository> mock)
+    {
+        mock.Setup(x => x.DeleteAsync(It.IsAny<Guid>()))
+            .ReturnsAsync((Guid roleId) =>
+            {
+                var role = _data.FirstOrDefault(r => r.Id == roleId);
+                if (role == null)
+                    return false;
+
+                var users = _userRoleRepository.GetUsersByRoleAsync(roleId).Result;
+                foreach (var user in users)
+                {
+                    _userRoleRepository.RemoveUserRoleAsync(user.Id, role.Id).Wait();
+                }
+
+                _data.Remove(role);
+                return true;
+            });
     }
 }
--- OnlineGameStore.BLL.Tests/RepositoryMockCreator/UserRepositoryMockCreator.cs
+++ OnlineGameStore.BLL.Tests/RepositoryMockCreator/UserRepositoryMockCreator.cs
@@ -1,3 +1,4 @@
+using Moq;
 using OnlineGameStore.DAL.Entities;
 using OnlineGameStore.DAL.Interfaces;
 
@@ -5,8 +6,31 @@ namespace OnlineGameStore.BLL.Tests.RepositoryMockCreator;
 
 public class UserRepositoryMockCreator : RepositoryMockCreator<User, IUserRepository>
 {
-    public UserRepositoryMockCreator(List<User> users)
+    private readonly IUserRoleRepository _userRoleRepository;
+
+    public UserRepositoryMockCreator(List<User> users, IUserRoleRepository userRoleRepository)
         : base(users)
     {
+        _userRoleRepository = userRoleRepository ?? throw new ArgumentNullException(nameof(userRoleRepository));
+    }
+
+    protected override void SetupDelete(Mock<IUserRepository> mock)
+    {
+        mock.Setup(x => x.DeleteAsync(It.IsAny<Guid>()))
+            .ReturnsAsync((Guid userId) =>
+            {
+                var user = _data.FirstOrDefault(u => u.Id == userId);
+                if (user == null)
+                    return false;
+
+                var roles = _userRoleRepository.GetUserRolesAsync(userId).Result;
+                foreach (var role in roles)
+                {
+                    _userRoleRepository.RemoveUserRoleAsync(user.Id, role.Id).Wait();
+                }
+
+                _data.Remove(user);
+                return true;
+            });
     }
 }
