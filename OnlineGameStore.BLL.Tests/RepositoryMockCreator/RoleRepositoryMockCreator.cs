35068c04be3f3072c859ee7fd11d2872da716566
Oleksii Stepanyk <o.stepanik06@gmail.com>
2025-06-19 22:05:56 +0300

    feature(bll-tests): removed unused mock

--- OnlineGameStore.BLL.Tests/RepositoryMockCreator/RoleRepositoryMockCreator.cs
+++ OnlineGameStore.BLL.Tests/RepositoryMockCreator/RoleRepositoryMockCreator.cs
@@ -13,24 +13,4 @@ public class RoleRepositoryMockCreator : RepositoryMockCreator<Role, IRoleReposi
     {
         _userRoleRepository = userRoleRepository ?? throw new ArgumentNullException(nameof(userRoleRepository));
     }
-
-    protected override void SetupDelete(Mock<IRoleRepository> mock)
-    {
-        mock.Setup(x => x.DeleteAsync(It.IsAny<Guid>()))
-            .ReturnsAsync( (Guid roleId) =>
-            {
-                var role = _data.FirstOrDefault(r => r.Id == roleId);
-                if (role == null)
-                    return false;
-
-                var users = _userRoleRepository.GetUsersByRoleAsync(roleId).Result;
-                foreach (var user in users)
-                {
-                    _userRoleRepository.RemoveUserRoleAsync(user.Id, role.Id).Wait();
-                }
-
-                _data.Remove(role);
-                return true;
-            });
-    }
 }
--- OnlineGameStore.BLL.Tests/RepositoryMockCreator/UserRepositoryMockCreator.cs
+++ OnlineGameStore.BLL.Tests/RepositoryMockCreator/UserRepositoryMockCreator.cs
@@ -13,24 +13,4 @@ public class UserRepositoryMockCreator : RepositoryMockCreator<User, IUserReposi
     {
         _userRoleRepository = userRoleRepository ?? throw new ArgumentNullException(nameof(userRoleRepository));
     }
-
-    protected override void SetupDelete(Mock<IUserRepository> mock)
-    {
-        mock.Setup(x => x.DeleteAsync(It.IsAny<Guid>()))
-            .Returns(async (Guid userId) =>
-            {
-                var user = _data.FirstOrDefault(u => u.Id == userId);
-                if (user == null)
-                    return false;
-
-                var roles = await _userRoleRepository.GetUserRolesAsync(userId);
-                foreach (var role in roles)
-                {
-                    await _userRoleRepository.RemoveUserRoleAsync(user.Id, role.Id);
-                }
-
-                _data.Remove(user);
-                return true;
-            });
-    }
 }
