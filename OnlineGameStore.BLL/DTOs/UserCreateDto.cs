6594df7f36fc14e1320ce5e79a220701ae9face8
Oleksii Stepanyk <o.stepanik06@gmail.com>
2025-06-19 15:05:36 +0300

    feature(bll): added DTOs for user

---
+++ OnlineGameStore.BLL/DTOs/UserCreateDto.cs
@@ -0,0 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace OnlineGameStore.BLL.DTOs;
+
+public class UserCreateDto
+{
+    [Required]
+    public required string Username { get; set; }
+    [Required]
+    public required string Email { get; set; }
+    [Required]
+    public required string Password { get; set; }
+}
---
+++ OnlineGameStore.BLL/DTOs/UserReadDto.cs
@@ -0,0 +1,7 @@
+namespace OnlineGameStore.BLL.DTOs;
+
+public class UserResponseDto
+{
+    public required string Username { get; set; }
+    public required string Email { get; set; }
+}
