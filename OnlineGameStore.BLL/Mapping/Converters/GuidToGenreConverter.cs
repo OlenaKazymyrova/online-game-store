0c6b8c7612c59b4ad99de8914db0ca5620650eb8
Herman Havva <108483440+hermanhavva@users.noreply.github.com>
2025-06-27 17:02:12 +0300

    add converters

--- OnlineGameStore.BLL/Mapping/Converters/GuidToGenreConverter.cs
+++ OnlineGameStore.BLL/Mapping/Converters/GuidToGenreConverter.cs
@@ -1,12 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using AutoMapper;
+using OnlineGameStore.DAL.DBContext;
+using OnlineGameStore.DAL.Entities;
 
-namespace OnlineGameStore.BLL.Mapping.Converters
+namespace OnlineGameStore.BLL.Mapping.Converters;
+
+public class GuidToGenreConverter : ITypeConverter<Guid, Genre>
 {
-    internal class GuidToGenreConverter
+    private readonly OnlineGameStoreDbContext _context;
+
+    public GuidToGenreConverter(OnlineGameStoreDbContext context)
     {
+        _context = context;
+    }
+
+    public Genre Convert(Guid source, Genre destination, ResolutionContext context)
+    {
+        var dbSet = _context.Set<Genre>();
+        var genre = dbSet.Find(source);
+        if (genre is not null)
+            return genre;
+
+        throw new KeyNotFoundException($"Genre with ID [{source}] was not found.");
     }
 }
--- OnlineGameStore.BLL/Mapping/Converters/GuidToPlatformConverter.cs
+++ OnlineGameStore.BLL/Mapping/Converters/GuidToPlatformConverter.cs
@@ -1,12 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using AutoMapper;
+using OnlineGameStore.DAL.DBContext;
 
-namespace OnlineGameStore.BLL.Mapping.Converters
+namespace OnlineGameStore.BLL.Mapping.Converters;
+public class GuidToPlatformConverter : ITypeConverter<Guid, Platform>
 {
-    internal class GuidToPlatfromConverter
+    private readonly OnlineGameStoreDbContext _context;
+
+    public GuidToPlatformConverter(OnlineGameStoreDbContext context)
     {
+        _context = context;
+    }
+
+    public Platform Convert(Guid source, Platform destination, ResolutionContext context)
+    {
+        var dbSet = _context.Set<Platform>();
+        var platform = dbSet.Find(source);
+        if (platform is not null)
+            return platform;
+
+        throw new KeyNotFoundException($"Platform with ID [{source}] was not found.");
     }
 }
