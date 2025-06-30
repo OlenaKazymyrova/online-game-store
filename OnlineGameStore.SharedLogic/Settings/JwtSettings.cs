7a9f78d15c7a65df94be1aa51a6714af8215c599
AnnN <anechytailenko@kse.org.ua>
2025-07-01 00:43:04 +0300

    20 min expiration

--- OnlineGameStore.SharedLogic/Settings/JwtSettings.cs
+++ OnlineGameStore.SharedLogic/Settings/JwtSettings.cs
@@ -3,5 +3,5 @@ namespace OnlineGameStore.SharedLogic.Settings;
 public static class JwtSettings
 {
     public const string SecretKey = "super-secure-jwt-key-with-33-bytes!!";
-    public const int ExpirationMinutes = 2;
+    public const int ExpirationMinutes = 20;
 }
